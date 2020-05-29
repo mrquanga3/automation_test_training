547394ba8d7b65f359f6782128eb6bd1ecebdb3a
tongnh <65882132+tongnh@users.noreply.github.com>
2020-05-29 11:32:35 +0700

    Fix Title

--- demos/src/demo/automation/Bai1.java
+++ demos/src/demo/automation/Bai1.java
@@ -1,17 +1,18 @@
-package A;
+package demo.automation;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Bai1 {
 		public static void main(String[] args) {
 
-//			 declaration and instantiation of objects/variables
-			System.setProperty("webdriver.chrome.driver","D:\\Eclip\\chromedriver.exe");
+//			 khai báo và khởi tạo đối tượng biến
+			//System.setProperty("webdriver.chrome.driver","D:\\Eclip\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
 
 			// String baseUrl = "http://demo.guru99.com/test/login.html";
 			driver.get("https://www.cmc.com.vn/");
 			driver.manage().window();
 			driver.close();
+			//quit() or close()
 		}
 }
 	
