854e391166241b6eb3abcda5204d308b6e6047d3
httdung <Jungthanhhoang@gmail.com>
2020-06-01 10:55:25 +0700

    add new bt1 httdung

---
+++ demos/httdung1/TestAutomation/src/com/cmc/automation/Bai1.java
@@ -0,0 +1,16 @@
+package com.cmc.automation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "E:\\SeleniumWebDriver\\chromedriver.exe");
+	    WebDriver driver= new ChromeDriver();
+	    driver.get("https://cmc.com.vn/");
+	    driver.manage().window().maximize();
+
+	}
+
+}
