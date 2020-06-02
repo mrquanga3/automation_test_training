593fb2d0744cdb73113b0cfd1d749ce572c735a9
ktthoa <khuongthoa16@gmail.com>
2020-06-03 00:22:00 +0700

    bai2.java

---
+++ demos/ktthoa/selenium/src/selenium/Motrang.java
@@ -0,0 +1,17 @@
+package selenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Motrang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// declaration and instantiation of objects/variables
+				//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+				driver.get("https://www.cmc.com.vn/");
+				driver.manage().window().maximize();
+				driver.close();
+
+	}
+
+}
