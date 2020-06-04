9b269125cf5d48973f39437c4c31c06b82ba688e
ptphuong1 <phuongpham.hvtc@gmail.com>
2020-06-04 10:03:03 +0700

    Update

--- demos/PTPhuong1/Selenium/Selenium_Bai tap 1/src/selenium/baiTap1.java
+++ demos/PTPhuong1/Selenium/Selenium_Bai tap 1/src/selenium/baiTap1.java
@@ -1,10 +1,6 @@
 package selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByCssSelector;
-import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class baiTap1 
