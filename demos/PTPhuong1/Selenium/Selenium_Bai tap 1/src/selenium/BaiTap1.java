2d0f5da2c7539b681e42c8aa782ab544277b9463
Quang Nguyen Dang <dangquangk53a3@gmail.com>
2020-06-05 10:31:28 +0700

    Update name

--- demos/PTPhuong1/Selenium/Selenium_Bai tap 1/src/selenium/BaiTap1.java
+++ demos/PTPhuong1/Selenium/Selenium_Bai tap 1/src/selenium/BaiTap1.java
@@ -3,11 +3,11 @@ package selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class baiTap1 
+public class BaiTap1 
 {
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.cmc.com.vn/");
 		driver.close();
