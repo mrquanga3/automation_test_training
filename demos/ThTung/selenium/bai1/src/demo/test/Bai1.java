680a2c74752b412bec6b86bc7101437ddb410c01
Quang Nguyen Dang <dangquangk53a3@gmail.com>
2020-06-05 10:20:37 +0700

    sua bai1

--- demos/ThTung/selenium/bai1/src/demo/test/Bai1.java
+++ demos/ThTung/selenium/bai1/src/demo/test/Bai1.java
@@ -3,14 +3,12 @@ package demo.test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class bai1 {
+public class Bai1 {
 	public static void main(String[] args) {
-
+		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.cmc.com.vn/");
 		driver.manage().window().maximize();
 		driver.close();
-
 	}
-
 }
