df6ff5804b907d6fac6050a52e778f4425ebe8fb
tongnh <65882132+tongnh@users.noreply.github.com>
2020-06-08 11:04:35 +0700

    Activity1

---
+++ demos/LGThang/SELENIUM/Activity1/src/demo/automation/openPage.java
@@ -0,0 +1,16 @@
+package demo.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openPage {
+	public static void main(String[] args) {
+
+		// khai báo và khởi tạo đối tượng biến
+			// System.setProperty("webdriver.chrome.driver","D:\\Eclip\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.cmc.com.vn/");
+			driver.manage().window().maximize();
+			driver.close();
+		}
+}
