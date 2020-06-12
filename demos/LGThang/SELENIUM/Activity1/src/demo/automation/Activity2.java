e6bcec8bcceb92c0b8fdd111ecef3f6de1d6408d
tongnh <65882132+tongnh@users.noreply.github.com>
2020-06-12 11:13:46 +0700

    update 1

--- demos/LGThang/SELENIUM/Activity1/src/demo/automation/Activity2.java
+++ demos/LGThang/SELENIUM/Activity1/src/demo/automation/Activity2.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,33 +17,35 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Activity2 {
 	static WebDriver browse;
+
 	@Before
-	public void demo(){
-	browse = new ChromeDriver();
-	browse.get("https://tuyendung.cmc.com.vn/");
-	browse.manage().window().maximize();
-	WebElement login = browse.findElement(By.xpath("//div[@class='menu-pc']//li[5]"));
-	login.click();
-	WebElement gg = browse.findElement(By.xpath("//div[@id='login_popup_id']//a[2]"));
-	gg.click();
-	WebElement account = browse.findElement(By.xpath("//input[@id='identifierId']"));
-	account.sendKeys("thangle123");
-	WebDriverWait wait = new WebDriverWait(browse,20);
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password']")));
-	WebElement pass = browse.findElement(By.xpath("//input[@name='password']"));
-	System.out.println(pass.getAttribute("value"));
-	pass.sendKeys("Legiathang1994");
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'p theo')]")));
-	WebElement nextpass = browse.findElement(By.xpath("//span[contains(text(),'p theo')]"));
-	nextpass.click();
-}
+	public void pre() {
+		browse = new ChromeDriver();
+		browse.get("https://tuyendung.cmc.com.vn/");
+		browse.manage().window().maximize();
+		WebElement login = browse.findElement(By.xpath("//div[@class='menu-pc']//li[5]"));
+		login.click();
+		WebElement gg = browse.findElement(By.xpath("//div[@id='login_popup_id']//a[2]"));
+		gg.click();
+		WebElement account = browse.findElement(By.xpath("//input[@id='identifierId']"));
+		account.sendKeys("sauriengj6");
+		WebDriverWait wait = new WebDriverWait(browse, 1);
+		/*
+		 * wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+		 * "//input[@name='password']")));
+		 */ WebElement pass = browse.findElement(By.xpath("//input[@name='password']"));
+		/* System.out.println(pass.getAttribute("value")); */
+		pass.sendKeys("123456aA@");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'p theo')]")));
+		WebElement nextpass = browse.findElement(By.xpath("//span[contains(text(),'p theo')]"));
+		nextpass.click();
+	}
 
 	@Test
 	public void testLogin() {
-		WebDriverWait wait = new WebDriverWait(browse, 20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-user-name-desktop")));
-		WebElement loged = browse.findElement(By.id("btn-user-name-desktop"));
-		assertTrue(loged.getText().equalsIgnoreCase("CHINHLAANHNHE"));
+		WebDriverWait wait = new WebDriverWait(browse, 5);
+		WebElement loged = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-user-name-desktop")));
+		assertTrue(loged.getText().equalsIgnoreCase("123456aA@"));
 	}
 
 	@Test
@@ -72,5 +75,8 @@ public class Activity2 {
 		ggs.click();
 	}
 
+	@After
+	public void close() {
+		browse.close();
+	}
 }
-
