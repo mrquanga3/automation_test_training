2b0bc26c748821c475c956b263df234d05122b25
Anh Nem <anhnem0708@gmail.com>
2020-06-16 00:05:51 +0700

    Dung commit bai selenium 3 chua lam xong

--- demos/nttdung/learn-selenium/src/TestNG/TestNG.java
+++ demos/nttdung/learn-selenium/src/TestNG/TestNG.java
@@ -1,62 +1,62 @@
-package TestNG;
-
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.BeforeClass;
-
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
-
-public class TestNG {
-	static WebDriver browser;
-
-	@BeforeMethod
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", System.getenv("webdriver.chrome.driver"));
-		browser = new ChromeDriver();
-		browser.manage().window().maximize();
-		browser.navigate().to("https://tuyendung.cmc.com.vn/");
-
-	}
-
-	@Test 
-	public void checkLogIn() {
-		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		String login = browser.findElement(By.xpath("//*[@id=\"btn-user-name-desktop\"]")).getText();
-		Assert.assertEquals("ANHNEM0708", login);
-		System.out.println(login);
-
-	}
-	
-	@Test 
-	public void checklogOut() {
-		// logout
-		checkLogIn();
-		WebElement hover = browser.findElement(By.xpath("//*[@id=\"btn-user-name-desktop\"]"));
-		Actions action = new Actions(browser);
-		action.moveToElement(hover).perform();
-		browser.findElement(By.xpath("//*[@id=\"logout_portal\"]")).click();
-
-		// check logout?
-		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		boolean logout = browser.findElement(By.xpath("//li[@class='nav-item work']//a[@class='btn-log login-portal']")).isDisplayed();
-		Assert.assertTrue(logout);
-		System.out.println("logout roi nha");
-
-	}
-
-}
+//package TestNG;
+//
+//import org.testng.annotations.Test;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.DataProvider;
+//import org.testng.annotations.BeforeClass;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.interactions.Actions;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeSuite;
+//import org.testng.annotations.AfterSuite;
+//
+//public class TestNG {
+//	static WebDriver browser;
+//
+//	@BeforeMethod
+//	public void setUp() {
+//		System.setProperty("webdriver.chrome.driver", System.getenv("webdriver.chrome.driver"));
+//		browser = new ChromeDriver();
+//		browser.manage().window().maximize();
+//		browser.navigate().to("https://tuyendung.cmc.com.vn/");
+//
+//	}
+//
+//	@Test 
+//	public void checkLogIn() {
+//		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		String login = browser.findElement(By.xpath("//*[@id=\"btn-user-name-desktop\"]")).getText();
+//		Assert.assertEquals("ANHNEM0708", login);
+//		System.out.println(login);
+//
+//	}
+//	
+//	@Test 
+//	public void checklogOut() {
+//		// logout
+//		checkLogIn();
+//		WebElement hover = browser.findElement(By.xpath("//*[@id=\"btn-user-name-desktop\"]"));
+//		Actions action = new Actions(browser);
+//		action.moveToElement(hover).perform();
+//		browser.findElement(By.xpath("//*[@id=\"logout_portal\"]")).click();
+//
+//		// check logout?
+//		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		boolean logout = browser.findElement(By.xpath("//li[@class='nav-item work']//a[@class='btn-log login-portal']")).isDisplayed();
+//		Assert.assertTrue(logout);
+//		System.out.println("logout roi nha");
+//
+//	}
+//
+//}
--- demos/nttdung/learn-selenium/src/com/cmc/TimKiemCongViec.java
+++ demos/nttdung/learn-selenium/src/com/cmc/TimKiemCongViec.java
@@ -1,6 +1,7 @@
 package com.cmc;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JList.DropLocation;
@@ -15,7 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TimKiemCongViec {
 	static WebDriver browser;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", System.getenv("webdriver.chrome.driver"));
 		browser = new ChromeDriver();
 		browser.manage().window().maximize();
@@ -26,49 +27,91 @@ public class TimKiemCongViec {
 //		inputfield.click();
 //		inputfield.sendKeys("Project Manager ahihi");
 //		browser.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
-		 /**
-	     * Filter by job
-	     * Click check all button
-	     *Expected: Check all the checkbox
-	   	  */
+		/**
+		 * Filter by job Click check all button Expected: Check all the checkbox
+		 */
 //		
 //		browser.findElement(By.xpath("//div[@class='search-list']//span[@class='multiselect-selected-text']")).click();
-//		browser.findElement(By.xpath("//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all']//input")).click();
-//		
+//		browser.findElement(
+//				By.xpath("//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all']//input"))
+//				.click();
+//
 //		List<WebElement> dropdownList = browser.findElements(By.xpath("//div[@class='search-list']//li"));
 //		boolean kiemtra = true;
-//		for (WebElement dropdown: dropdownList) {
+//		for (WebElement dropdown : dropdownList) {
 //			if (dropdown.getAttribute("class") == null || !dropdown.getAttribute("class").contains("active")) {
 //				kiemtra = false;
-//				break;	
+//				break;
 //			}
 //		}
 //		Assert.assertTrue(kiemtra);
 //		System.out.println("chon ca rui nha");
-		 /**
-	     * Filter by job
-	     * Click check all button
-	     * Un-check ant value of the dropdown list
-	     *Expected: the check all icon is not selected.
-	   	  */
-		
-		browser.findElement(By.xpath("//div[@class='search-list']//span[@class='multiselect-selected-text']")).click();
-		WebElement checkAll = browser.findElement(By.xpath("//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all']//input"));
-		for (int i = 0; i<2; i++) {
-			checkAll.click();
-			System.out.println(checkAll.isSelected());
-		}
-		
-		
-//		Boolean numberselect = browser.findElement(By.xpath("//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all active']//input")).isSelected();
+
+		/**
+		 * Filter by job 
+		 * Click check all button. Un-check the check all button 
+		 * Expected: the check all icon is not selected.
+		 */
+
+//		browser.findElement(By.xpath("//div[@class='search-list']//span[@class='multiselect-selected-text']")).click();
+//		WebElement checkAll = browser.findElement(By.xpath("//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all']//input"));
+//		for (int i = 0; i<2; i++) {
+//			checkAll.click();
+//			System.out.println(checkAll.isSelected());
+//			browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		}
+
+		/**
+		 * Filter by job 
+		 * Click check all button, after that un-check any value of the dropdown list 
+		 * Expected: the check all icon is not selected.
+		 */
+//		
+//		browser.findElement(By.xpath("//div[@class='search-list']//span[@class='multiselect-selected-text']")).click();
+//		browser.findElement(
+//				By.xpath("//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all']//input"))
+//				.click();
+//		Random ran = new Random();
+//		int i = ran.nextInt(29);
+//		Thread.sleep(2000);
+//		browser.findElement(By.xpath("//div[@class='btn-group open']//li[" + 5 + "]//input")).click();
+//		Boolean numberselect = !browser
+//				.findElement(By.xpath(
+//						"//div[@class='btn-group open']//li[@class='multiselect-item multiselect-all']//input"))
+//				.isSelected();
 //		Assert.assertTrue(numberselect);
-//		System.out.println("chon ca rui nha");
+//		System.out.println("bo chon ca rui nha");
+//		
+		/**
+		 * Filter by job 
+		 * Click to any value of the dropdown list
+		 * Expected: the selected value is not selected.
+		 */
+		browser.findElement(By.xpath("//div[@class='search-list']//span[@class='multiselect-selected-text']")).click();
+		Random ran1 = new Random();
+		int i1 = ran1.nextInt(29);
+		Thread.sleep(2000);
+		browser.findElement(By.xpath("//div[@class='btn-group open']//li[" + i1 + "]//input")).click();
+		WebElement select1Value = browser.findElement(By.xpath("//div[@class='search-list']//li[" +  i1  + "]"));
+		boolean check = true;
+		if (select1Value.getAttribute("class") == null || !select1Value.getAttribute("class").contains("active")) {
+		check = false;
+		}
+		Assert.assertTrue(check);
+		System.out.println("Chon 1");
 		
 		
+
+//		browser.findElement(By.className("multiselect-container dropdown-menu")).findElement(By.className("checkbox"));
+
 //		browser.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
 //		System.out.println("ok");
-//		Assert.assertEquals("Không tìm thấy nội dung phù hợp", message);
-//		System.out.println(message);
+//		WebElement message = browser.findElement(By.xpath("//div[@class='content-main']//h5"));
+//		if (message != null) {
+//			String noimia = message.getText();
+//			Assert.assertEquals("Không tìm thấy nội dung phù hợp", noimia);
+//			System.out.println(noimia);
+//		}
 
 	}
 
