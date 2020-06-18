5e825f8bd0087c843265d44928bc263d3c7d3cdd
tongnh <65882132+tongnh@users.noreply.github.com>
2020-06-18 11:26:36 +0700

    Update

--- demos/LGThang/SELENIUM/Activity1/src/demo/automation/AppTest.java
+++ demos/LGThang/SELENIUM/Activity1/src/demo/automation/AppTest.java
@@ -1,5 +1,7 @@
 package demo.automation;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,14 +11,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
 static WebDriver driver;
+static WebElement searchButton;
 @Before
 public void setup() {
 	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://tuyendung.cmc.com.vn/");
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	searchButton = driver.findElement(By.xpath("//div[@class='search-list']//span[@class='multiselect-native-select']"));
 }
 @Test
 public void test() {
-	WebElement radio = driver.findElement(By.id("checkbox"));
+	
 }
 }
