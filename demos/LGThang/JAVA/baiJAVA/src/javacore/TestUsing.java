46bb327f6f05b5816d9c7e0b57900121bcb93615
Quang Nguyen Dang <dangquangk53a3@gmail.com>
2020-06-23 10:20:55 +0700

    Update reading file

--- demos/LGThang/JAVA/baiJAVA/src/javacore/TestUsing.java
+++ demos/LGThang/JAVA/baiJAVA/src/javacore/TestUsing.java
@@ -3,49 +3,52 @@ package javacore;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.theories.DataPoint;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.json.Json;
-import org.testng.annotations.DataProvider;
 
 public class TestUsing {
 	WebDriver driver;
 
 	@Before
-	void setup() {
+	public void setup() {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MICROSECONDS);
 	}
 
 	@After
-	void close() {
+	public void close() {
 		driver.close();
 	}
 
 	@Test
-	void login() {
+	public void login() throws IOException {
+		String account = readUsernamePassword();
+		String username = account.split(";")[0];
+		String password = account.split(";")[1];
 		driver.get(
 				"https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys(username);
 	}
 
-	void readTxt(){
-		  File file = new File("Test.txt");
-	        FileInputStream IP = new FileInputStream(file);
-	        InputStreamReader inputStreamReader = new InputStreamReader(IP);
-	        BufferedReader reader = new BufferedReader(inputStreamReader);
-	        String line = "";
-	        while ((line = reader.readLine()) != null) {
-	            System.out.println(line);
+	String readUsernamePassword() throws IOException {
+		File file = new File("src\\javacore\\Test.txt");
+		FileInputStream IP = new FileInputStream(file);
+		InputStreamReader inputStreamReader = new InputStreamReader(IP);
+		BufferedReader reader = new BufferedReader(inputStreamReader);
+		String account = "";
+		while ((account = reader.readLine()) != null) {
+			System.out.println(account);
+			break;
+		}
+		reader.close();
+		return account;
 	}
-	}
-
+}
