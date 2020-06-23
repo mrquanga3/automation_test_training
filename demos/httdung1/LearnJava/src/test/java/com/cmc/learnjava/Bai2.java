fe54908667cdef0ea8c21bfeea5f90e061f5190e
httdung <Jungthanhhoang@gmail.com>
2020-06-23 09:04:34 +0700

    Add Bai 3

--- demos/httdung1/LearnJava/src/test/java/com/cmc/learnjava/Bai2.java
+++ demos/httdung1/LearnJava/src/test/java/com/cmc/learnjava/Bai2.java
@@ -1,36 +1,36 @@
 package com.cmc.learnjava;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class Bai2 {
 
 	@Test
-	public void checkTongInt(){
+	public void checkSumInt(){
 		TinhTong tinhTong2 = new TinhTong(5,4);
 		int ketQua = tinhTong2.tinhTong2();		
 		assertTrue(ketQua == 9);
 	}
 	
 	@Test
-	public void checkTongFloat(){
+	public void checkSumFloat(){
 		TinhTong tinhTong1 = new TinhTong(2.5f, 8.5f);
 		float ketQua = tinhTong1.tinhTong1();		
 		assertTrue(ketQua == 11f);
 	}
 	
 	@Test
-	public void checkTrungBinhInt(){
+	public void checkAverageInt(){
 		TinhTong tinhTong2 = new TinhTong(6,4);
-		float ketQua = tinhTong2.tinhTb2();		
+		float ketQua = tinhTong2.tinhTrungBinhCong2();		
 		assertTrue(ketQua == 5);
 	}
 	
 	@Test
-	public void checkTrungBinhFloat(){
+	public void checkAverageFloat(){
 		TinhTong tinhTong1 = new TinhTong(2.5f, 8.5f);
-		float ketQua = tinhTong1.tinhTb1();		
+		float ketQua = tinhTong1.tinhTrungBinhCong1();		
 		assertTrue(ketQua == 5.5f);
 	}
 	
