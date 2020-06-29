528dc494399aa1e56e81fb94ea1e97ad64beb936
Le Gia Thang <lethang098194@gmail.com>
2020-06-29 21:27:10 +0700

    update Activity4

--- demos/LGThang/JAVA/src/JavaBasic/Activity4/HomePage.java
+++ demos/LGThang/JAVA/src/JavaBasic/Activity4/HomePage.java
@@ -1,5 +1,36 @@
 package JavaBasic.Activity4;
 
-public class HomePage {
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePage extends Login {
+	@Test
+	public void testLogin() {
+//		login();
+//		WebDriverWait wait = new WebDriverWait(driver, 5);
+//		WebElement loged = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-user-name-desktop")));
+//		Assert.assertTrue(loged.getText().equalsIgnoreCase("LETHANG098194@GMAIL.COM"));
+		login();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String txtlogin = driver.findElement(By.xpath("//*[@id=\"btn-user-name-desktop\"]")).getText();
+		assertEquals(txtlogin, "lETHANG098194");
+	}
+
+	@Test
+	public void testLogout() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		logout();
+		WebElement lgu = driver.findElement(By.xpath("//a[@id='logout_portal']"));
+		lgu.click();
+	
+	}
 }
--- demos/LGThang/JAVA/src/JavaBasic/Activity4/Login.java
+++ demos/LGThang/JAVA/src/JavaBasic/Activity4/Login.java
@@ -4,7 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,7 +18,7 @@ public class Login {
 
 	@BeforeClass
 	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lgthang\\Desktop\\chromedriver_win32_V83\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32_v83\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://tuyendung.cmc.com.vn/");
@@ -23,7 +27,25 @@ public class Login {
 
 	@Test
 	public void login() {
-		driver.findElement(By.xpath("//a[contains(text(),'ng Nh')]")).click();
+		driver.findElement(By.xpath("//li[@class='nav-item work']//a[@class='btn-log login-portal']")).click();
+		driver.findElement(By.xpath("//*[@id=\"login_popup_id\"]//a[@href='/login-google']")).click();
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("lethang098194@gmail.com");
+		WebElement next = driver.findElement(By.xpath("//*[contains(text(),'Tiếp theo')]"));
+		next.click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Legiathang");
+		WebDriverWait  wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Tiếp theo')]")));
+		WebElement next1 =driver.findElement(By.xpath("//*[contains(text(),'Tiếp theo')]"));
+		next1.click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+	public void logout() {
+		Actions action = new Actions(driver);
+		WebElement lgo = driver.findElement(By.xpath("//a[@id='btn-user-name-desktop']"));
+		action.moveToElement(lgo).build().perform();		
+		driver.findElement(By.xpath("//a[@id='logout_portal']")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@AfterClass
