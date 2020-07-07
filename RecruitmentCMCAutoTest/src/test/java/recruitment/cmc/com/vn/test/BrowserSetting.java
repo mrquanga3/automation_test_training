a4d006ddd92be4c23da92b3b573cf1693393e032
anhnem <thanhdungnt88@gmail.com>
2020-07-07 10:08:40 +0700

    Dung up phan set up browser

--- RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/BrowserSetting.java
+++ RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/BrowserSetting.java
@@ -6,19 +6,63 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserSetting {
 	public WebDriver BrowserSettings() {
 		WebDriver driver;
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		driver.get(BASE_URL);
+
+		/**
+		 * This function will execute before each Test tag in testng.xml
+		 * @param browser
+		 * @throws Exception
+		 */
+		@BeforeTest
+		@Parameters("browser")
+		public void setup(String browser) throws Exception{
+			//Check if parameter passed from TestNG is 'firefox'
+			if(browser.equalsIgnoreCase("firefox")){
+			//create firefox instance
+				System.setProperty("webdriver.gecko.driver", System.getenv("webdriver.firefox.driver"));
+				driver = new FirefoxDriver();
+			}
+			//Check if parameter passed as 'chrome'
+			else if(browser.equalsIgnoreCase("chrome")){
+				//set path to chromedriver.exe
+				System.setProperty("webdriver.chrome.driver",System.getenv("webdriver.chrome.driver"));
+				//create chrome instance
+				driver = new ChromeDriver();
+			}
+//			//Check if parameter passed as 'Edge'
+//					else if(browser.equalsIgnoreCase("Edge")){
+//						//set path to Edge.exe
+//						System.setProperty("webdriver.edge.driver",".\\MicrosoftWebDriver.exe");
+//						//create Edge instance
+//						driver = new EdgeDriver();
+//					}
+			else{
+				//If no browser passed throw exception
+				throw new Exception("Browser is not correct");
+			}
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+		
+		@Test
+		public void testBrowser() throws InterruptedException{
+			browser.navigate().to("https://tuyendung.cmc.com.vn/");
+			browser.findElement(By.xpath("//li[@class='nav-item work']//a[@class='btn-log login-portal']")).click();
+			browser.findElement(By.xpath("//*[@id=\"login_popup_id\"]/div/div/div[2]/div[2]/div[2]/div/a[2]/i")).click();
+			browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			//Fill user name
+			browser.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("anhnem0708@gmail.com");
+			browser.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+			//Fill pass
+			browser.findElement(By.xpath("//input[@name='password']")).sendKeys("123456oC!");
+			browser.findElement(By.xpath("//*[@id=\"passwordNext\"]/span/span")).click();
+		}
 		return driver;
 	}
 }
