837ac3642a0d1c50a8785fee6363aff099b9e032
ptphuong1 <phuongpham.hvtc@gmail.com>
2020-07-10 10:57:39 +0700

    Test slider

--- RecruitmentCMCAutoTest/src/main/java/recruitment/cmc/com/pages/HomePage.java
+++ RecruitmentCMCAutoTest/src/main/java/recruitment/cmc/com/pages/HomePage.java
@@ -1,12 +1,54 @@
 package recruitment.cmc.com.pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import recruitment.cmc.com.settings.Constant;
 
 public class HomePage extends BasePage {
-
+	@FindBy(how = How.CSS, using =".carousel-item.active .mask-banner img")
+	WebElement slideActive;
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
-	// slide
-	// next back
+	
+	// Get src slide
+	public List<WebElement> EnnoughSlide(){
+		//lay list 3 anh hien thi tren slide
+		List<WebElement> listElement = driver.findElements(By.cssSelector(".home-page-slider .carousel-item .mask-banner img"));
+		int size = listElement.size();
+		System.out.println(size);
+		//in ten cai slide active
+		System.out.println(slideActive.getAttribute("src"));
+		//kiem tra image1 cos nam trong list image vua lay duoc khong
+		for (int i = 0; i < listElement.size(); i++) {
+			if(Constant.name_image1.equals(listElement.get(i).getAttribute("src"))) {
+				System.out.println("Enough slide");
+			}
+		}
+		return listElement;
+	}
+
+//	Next slide
+	public void NextSlide() {
+		WebElement slidePageActive1 = driver.findElement(By.xpath("*[@id=\"carousel-example-generic\"]/div/div[1]/div[2]/img"));
+		driver.findElement(By.xpath("//*[@id=\"carousel-example-generic\"]/a[2]")).click();
+		WebElement slidePageActive2 = driver.findElement(By.xpath("/html/body/div[4]/section/section/div/div/div[3]/div[2]/img"));
+		driver.findElement(By.xpath("//*[@id=\"carousel-example-generic\"]/a[2]")).click();
+		WebElement slidePageActive3 = driver.findElement(By.xpath("/html/body/div[4]/section/section/div/div/div[2]/div[2]/img"));
+	}
+	
+	// Back slide
+	public void BackSlide() {
+		WebElement slidePageActive1 = driver.findElement(By.xpath("*[@id=\"carousel-example-generic\"]/div/div[1]/div[2]/img"));
+		driver.findElement(By.xpath("//*[@id=\"carousel-example-generic\"]/a[1]/span[1]")).click();
+		WebElement slidePageActive2 = driver.findElement(By.xpath("/html/body/div[4]/section/section/div/div/div[2]/div[2]/img"));
+		driver.findElement(By.xpath("//*[@id=\"carousel-example-generic\"]/a[1]/span[1]")).click();
+		WebElement slidePageActive3 = driver.findElement(By.xpath("/html/body/div[4]/section/section/div/div/div[3]/div[2]/img"));
+	}	
 }
--- RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/TestHomePage.java
+++ RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/TestHomePage.java
@@ -3,6 +3,9 @@ package recruitment.cmc.com.vn.test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import recruitment.cmc.com.pages.HomePage;
@@ -18,5 +21,24 @@ public class TestHomePage extends TestTemplate {
 	public void testLogOut() {
 		HomePage tlgOut = new HomePage(driver);
 		assertNotEquals(tlgOut.logOut(), "TUNGTHSE02891");
+	}
+	
+	@Test
+	public void TestEnoughSlide() {
+		HomePage homepag = new HomePage(driver);
+		homepag.EnnoughSlide();
+		assertEquals(homepag.EnnoughSlide().size(), 3);
+	}
+	
+	@Test
+	public void TestNextSlide() {
+		HomePage homepag = new HomePage(driver);
+		homepag.NextSlide();
+	}
+	
+	@Test
+	public void TestBackSlide() {
+		HomePage homepag = new HomePage(driver);
+		homepag.BackSlide();
 	}
 }
