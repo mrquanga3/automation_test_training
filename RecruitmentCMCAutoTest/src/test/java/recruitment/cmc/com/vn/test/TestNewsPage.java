a49375991a1d566efebeebc3250da6e3a3ca6fcc
Quang NGUYEN DANG <dangquangk53a3@gmail.com>
2020-07-29 15:35:28 +0700

    run parallel

--- RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/TestNewsPage.java
+++ RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/TestNewsPage.java
@@ -9,54 +9,41 @@ import recruitment.cmc.com.pages.NewsPage;
 import recruitment.cmc.com.settings.NewsInfo;
 
 public class TestNewsPage extends TestTemplate {
-	@Test 
-	public void testLikeNotLogged() {
-		NewsPage newsPage = new NewsPage(driver);
-		assertEquals("Bạn chưa đăng nhập", newsPage.pressLikeButton());
-	}
+	static NewsPage newsPage;
 
-	@Test 
-	public void testLikeLogged() {
-		NewsPage newsPage = new NewsPage(driver);
-		assertEquals("Đã thích", newsPage.pressLikeButtonLogged());
-	}
-	
-	@Test 
-	public void testUnLikeLogged() {
-		NewsPage newsPage = new NewsPage(driver);
-		assertEquals("Yêu thích", newsPage.pressUnLikeButtonLogged());
-	}	
-	
-	// Begin of dunghtt1	
-	// Save case test data to object array for method verifyDisplayListOfNews()
+	/*
+	 * @Test public void testLikeNotLogged() { newsPage = new NewsPage(driver);
+	 * assertEquals("Bạn chưa đăng nhập", newsPage.pressLikeButton()); }
+	 * 
+	 * @Test public void testLikeLogged() { newsPage = new NewsPage(driver);
+	 * assertEquals("Đã thích", newsPage.pressLikeButtonLogged()); }
+	 * 
+	 * @Test public void testUnLikeLogged() { newsPage = new NewsPage(driver);
+	 * assertEquals("Yêu thích", newsPage.pressUnLikeButtonLogged()); }
+	 */
+	// Begin of dunghtt1
+	// Save case test data to object array
 	@DataProvider(name = "newslistdata")
 	public NewsInfo[] newsListDataprovider() throws Exception {
-		NewsPage newsP = new NewsPage(driver);				
-		return newsP.getListNewsFromFile();
+		newsPage = new NewsPage(driver);
+		return newsPage.getListNewsFromFile();
 	}
-	
-	//Verify display list of the News use method newsListDataprovider()
+
+	// Verify display list of the News
 	@Test(dataProvider = "newslistdata")
-	public void verifyDisplayListOfNews(NewsInfo sNews) throws Exception{				
-		  NewsPage newsP = new NewsPage(driver); 
-		  String resultTest = newsP.getStatusOfNewsList(sNews.subTitle, sNews.urlBanner, sNews.subContent, sNews.detailContent, sNews.postDate); 
-		  assertEquals(resultTest, "Display correct the news");		 
-	}
-	
-	//Verify display list of the News use method getListNews()
-	@Test(dataProvider = "newslistdataA")
-	public void verifyDisplayListOfNewsA(String subTitle, String urlBanner, String subContent, String detailContent, String postDate) throws Exception {
-		NewsPage newsP = new NewsPage(driver);
-		String resultTest = newsP.getStatusOfNewsList(subTitle, urlBanner, subContent, detailContent, postDate);
+	public void verifyDisplayListOfNews(NewsInfo sNews) throws Exception {
+		newsPage = new NewsPage(driver);
+		String resultTest = newsPage.getStatusOfNewsList(sNews.subTitle, sNews.urlBanner, sNews.subContent,
+				sNews.detailContent, sNews.postDate);
 		assertEquals(resultTest, "Display correct the news");
 	}
-	
-	//Verify display the detail of the News
+
+	// Verify display the detail of the News
 	@Test
-	public void verifyDisplayDetailOfNews() throws Exception{				
-		  NewsPage newsP = new NewsPage(driver);		 
-		  String resultTest = newsP.getStatusDetailOfNews(); 
-		  assertEquals(resultTest, "Display correct the news");		 
+	public void verifyDisplayDetailOfNews() throws Exception {
+		newsPage = new NewsPage(driver);
+		String resultTest = newsPage.getStatusDetailOfNews();
+		assertEquals(resultTest, "Display correct the news");
 	}
 	// End of dunghtt1
 }
--- RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/TestTemplate.java
+++ RecruitmentCMCAutoTest/src/test/java/recruitment/cmc/com/vn/test/TestTemplate.java
@@ -1,21 +1,37 @@
 package recruitment.cmc.com.vn.test;
 
+import java.util.Set;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 
 public class TestTemplate {
 	BrowserSetting bs;
 	WebDriver driver;
 
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	public void setup() throws Exception {
 		bs = new BrowserSetting();
 		driver = bs.BrowserSettings();
 	}
 
-	@AfterMethod
+	@AfterMethod(alwaysRun = true)
 	public void tearnDown() {
+		//if (driver != null)
+		//	driver.quit();
+		//driver = null;
+		// Set<String> handles = driver.getWindowHandles();
+		// for (String handle : handles) {
+		// driver.switchTo().window(handle);
+		// driver.close();
+		// }
+		// driver.close();
+	}
+
+	@AfterSuite
+	public void tearnDownSuite() {
 		driver.quit();
 	}
 }
